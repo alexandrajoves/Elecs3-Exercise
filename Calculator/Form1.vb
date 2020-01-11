6807d024e01dca7b95c113bbb88ed4e34aefe2b6
Alexandra Joves <jovesalexandra@gmail.com>
2020-01-11 12:59:48 +0800

    initial commit entry

---
+++ Calculator/Form1.vb
@@ -0,0 +1,126 @@
+﻿Public Class Form1
+    Private flag = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        TextBox1.Text = TextBox1.Text & "0"
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "1"
+            flag = True
+        ElseIf flag = True Then
+        End If
+        TextBox1.Text = TextBox1.Text & "1"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "2"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "2"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "3"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "3"
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "4"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "4"
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "5"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "5"
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "6"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "6"
+        End If
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "7"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "7"
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "8"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "8"
+        End If
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "9"
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "9"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & "."
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & "."
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If flag = False Then
+            TextBox1.Text = ""
+            TextBox1.Text = TextBox1.Text & ","
+            flag = True
+        ElseIf flag = True Then
+            TextBox1.Text = TextBox1.Text & ","
+        End If
+    End Sub
+End Class
